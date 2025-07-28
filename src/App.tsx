32df60b3292fd861305df1af6206b0e183a86c1a
Phillip <contato.phillip.menezes@gmail.com>
2025-07-28 17:19:44 -0300

    chore: react-router-dom added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import Counter from '@components/Counter'
 import { DarkTheme, LightTheme } from '@styles/theme'
 import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import ThemeButton from './components/ThemeButton'
+import Home from './pages/Home'
 import GlobalStyle from './styles/reset'
 
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(false)
   return (
-    <ThemeProvider theme={isDarkTheme ? DarkTheme : LightTheme}>
-      <GlobalStyle />
-      <ThemeButton onClick={() => setIsDarkTheme(!isDarkTheme)} />
-      <Counter />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={isDarkTheme ? DarkTheme : LightTheme}>
+        <GlobalStyle />
+        <ThemeButton onClick={() => setIsDarkTheme(!isDarkTheme)} />
+        <Home />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import Counter from '@/components/Counter'
+
+const Home = () => {
+  return (
+    <>
+      <h1>HomeApp</h1>
+      <Counter />
+    </>
+  )
+}
+export default Home
