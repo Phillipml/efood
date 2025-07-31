3345e6aa25a1f89529b1b12d9a5d3cc856e975d1
Phillip <contato.phillip.menezes@gmail.com>
2025-07-31 11:40:44 -0300

    fix: vitest removido por não usabilidade no projjeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-/// <reference types="vitest/config" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
-// https://vite.dev/config/
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-const dirname =
-  typeof __dirname !== 'undefined'
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url))
-
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [
     react(),
@@ -44,33 +33,5 @@ export default defineConfig({
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     'process.env.VITE_APP_TITLE': JSON.stringify(process.env.VITE_APP_TITLE)
-  },
-  test: {
-    projects: [
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({
-            configDir: path.join(dirname, '.storybook')
-          })
-        ],
-        test: {
-          name: 'storybook',
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: 'playwright',
-            instances: [
-              {
-                browser: 'chromium'
-              }
-            ]
-          },
-          setupFiles: ['.storybook/vitest.setup.ts']
-        }
-      }
-    ]
   }
 })
