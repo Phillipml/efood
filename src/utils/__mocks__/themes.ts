698d4b315a3501affa031068a9c05548d44b6031
Phillipml <contato.phillip.menezes@gmail.com>
2025-09-18 18:32:58 -0300

    feat: color-utils test

---
+++ src/utils/__mocks__/themes.ts
@@ -0,0 +1,14 @@
+export const mockLightTheme = {
+  primary: '#FFF8F2',
+  secondary: '#FFEBD9',
+  tertiary: '#E66767',
+  quaternary: '#FFB930',
+  quinary: '#FFFFFF'
+}
+export const mockDarkTheme = {
+  primary: '#4B0D0D',
+  secondary: '#E66767',
+  tertiary: '#FFEBD9',
+  quaternary: '#FFB930',
+  quinary: '#FFF8F2'
+}
